b178566a38acc04941f819e2cc604dbc13af9545
DAMPFERS <vlad.razyvaev.2805@mail.ru>
2024-09-22 23:14:39 +0700

    start crawler

--- spider.py
+++ spider.py
@@ -5,10 +5,17 @@ link = "https://www.nstu.ru/"
 response = requests.get(link).text
 soup = BS(response, "lxml")
 
-print(soup.find("div", class_ = "main-events__grid js-main-events-grid").get_text())
+
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+
+
+
+
+# print(soup.find("div", class_ = "main-events__grid js-main-events-grid").get_text())
 # print(soup.get_text())
-# for link in soup.find_all('a'):
-    # print(link.get('href'))
 
 # print(response.status_code)
 # print(response.text)
