00d5534cbfbb28db63f5f088dde595042b1c60aa
DAMPFERS <vlad.razyvaev.2805@mail.ru>
2024-09-23 16:03:19 +0700

    check browsing_history

--- spider.py
+++ spider.py
@@ -2,32 +2,6 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 
-
-#link_elements = soup.select("a[href]")
-
-# urls = []
-# for link_element in link_elements:
-#     url = link_element["href"]
-#     if "https:" in url:
-#         urls.append(url)
-
-# print(urls)
-
-
-
-
-
-
-# print(soup.find("div", class_ = "main-events__grid js-main-events-grid").get_text())
-# print()
-
-# print(response.status_code)
-# print(response.text)
-
-
-
-
-
 def _addrVerification(origin_url: str, addr: str) -> str:
     if "https://" in addr:
         prohibited_words = [".pdf", ".png", ".jpg", ".docx", ".xmls", "youtube", "tiktok", "rutube", "ok.ru", "t.me/", "zen.yandex.ru", "gosuslugi.", "vk.com"]  
@@ -36,7 +10,6 @@ def _addrVerification(origin_url: str, addr: str) -> str:
         return addr 
     elif addr[0] == '/': return str(origin_url) + addr
     else: return None
-
 
 
 def getPage(url: str) -> dict:
@@ -48,7 +21,7 @@ def getPage(url: str) -> dict:
     soup = BS(response.content, "lxml")
 
     text = soup.get_text()
-    
+
     urls = []
     for link in soup.find_all('a'):
         addr = link.get("href")
@@ -57,24 +30,21 @@ def getPage(url: str) -> dict:
     return {"Text": text, "Links": urls}
         
 
+
 def main():
-    DEEP = 2
+    DEEP = 1
     current_URLs = ["https://www.nstu.ru/"]
-    url_history = []
+    browsing_history = [] 
     for _ in range(DEEP):
         next_URLs = []
         for url in current_URLs:
             page = getPage(url)
+            browsing_history.append(url)
             for link in page["Links"]:
-                if link not in url_history: 
-                    url_history.append(link)
-                    next_URLs.append(link)
+                if link not in browsing_history: next_URLs.append(link)
         current_URLs = next_URLs
         
 
 
 if __name__ == "__main__":
-    a = ["qwe"]
-    b = ["qax", "qwe"]
-    print(a[0] in b)
-    # main()
+    main()
